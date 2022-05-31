93533fcd3014b056c19fbaa37ab0d5192f9f4101
TeChRF24 <f.rafael.temich24@gmail.com>
2022-05-31 12:25:24 -0500

    Adding more dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,22 @@
-const { src, dest } = require('gulp');
+const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 
 function css(done) {
     // Compilar sass
     // Pasos: 1 - identificar archivo, 2 - Compilarla, 3 - Guardar el .css
     src('src/scss/app.scss')
-        .pipe( sass() )
+        .pipe( sass({outputStyle: 'compressed'}) )
+        .pipe( postcss([ autoprefixer() ]) )
         .pipe( dest('build/css') )
 
     done();
 }
 
+function dev() {
+    watch('src/scss/app.scss', css);
+}
+
 exports.css = css;
+exports.dev = dev;

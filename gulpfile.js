e687d04578f9267c3cbee4cb7bdf6f47c313f70c
TeChRF24 <f.rafael.temich24@gmail.com>
2022-06-02 01:31:49 -0500

    Done setting up gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { src, dest, watch } = require('gulp');
+const { src, dest, watch, series, parallel } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
@@ -15,8 +15,13 @@ function css(done) {
 }
 
 function dev() {
-    watch('src/scss/app.scss', css);
+    watch('src/scss/**/*.scss', css);
 }
+
 
 exports.css = css;
 exports.dev = dev;
+exports.default = series(css, dev);
+
+// series - Se inicia una tarea y hasta que finaliza, inicia la siguiente
+// parallel - Todas inician al mismo tiempo
